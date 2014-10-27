efce43da678879176595c70fb03344a93e8047dd
Duncan Smith <duncsmit@umich.edu>
2014-10-27 15:21:21 -0400

    Arrows added

---
+++ Assets/Scripts/ArrowScript.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ArrowScript : MonoBehaviour {
+
+	public Transform ship1;
+	public Transform ship2;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector3 arrowAxis = (ship1.position - ship2.position).normalized;
+		float arrowAngle = Vector3.Angle (ship1.position, ship2.position);
+//		transform.Rotate (arrowAxis, arrowAngle);
+
+		transform.rotation = Quaternion.LookRotation (arrowAxis);
+//		transform.rotation = transform.rotation.ToAngleAxis(arrow
+//		transform.rotation = Quaternion.RotateTowards (transform.rotation, Quaternion.LookRotation(arrowAxis), arrowAngle);
+		transform.position = ship1.position;
+	}
+}
