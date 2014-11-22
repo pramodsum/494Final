a4858ac188d39ada0f1d29421b29004b594cabbb
Duncan Smith <duncsmit@umich.edu>
2014-11-21 19:47:32 -0500

    Stations can now be captured back

--- Assets/Scripts/Station.cs
+++ Assets/Scripts/Station.cs
@@ -6,7 +6,7 @@ public class Station : MonoBehaviour
 		public Station_Control station;
 		void OnTriggerEnter (Collider other)
 		{
-				if (other.gameObject.name == "Shot" && !station.inControl) {
+				if (other.gameObject.name == "Shot" /*&& !station.inControl*/) {
 						this.renderer.material.color = other.renderer.material.color; 
 				}
 		}
