1c060a3d162bd012724666f76786539bc1b17da4
twlamere <twlamere@gmail.com>
2014-11-22 19:08:26 -0500

    controllers should work now
    The controllers should work for both the mac and pc controllers now.
    Controls are:
    boost = A
    break = B
    shoot = left or right bumpers
    
    Also, I changed up the left and right joysticks. Let me know how you all
    like it. If not enough controllers are plugged in, the default is to use
    the keyboard for the rest. @duncsmit @pramodsum @EvanHahn

--- Assets/Scripts/Shot Scripts/Shot.cs
+++ Assets/Scripts/Shot Scripts/Shot.cs
@@ -1,50 +1,63 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class Shot : MonoBehaviour {
-	public GameObject explosion;
+public class Shot : MonoBehaviour
+{
+		public GameObject explosion;
 
-	private Vector3 direction;
-	private Vector3 velocity;
+		private Vector3 direction;
+		private Vector3 velocity;
 
-	public float maxDist = 10f;
+		public float maxDist = 1200f;
 
-	private Vector3 startPosition;
-	private GameObject shooter;
-	private GameObject target;
+		private Vector3 startPosition;
+		private GameObject shooter;
+		private GameObject target;
 
-	public float lockOnDistance = 100;
+		public float lockOnDistance = 100;
 
 
-	public bool homing;
+		public bool homing;
 
-	void Start() {
-        startPosition = transform.position; 
-	}
+		void Start ()
+		{
+				startPosition = transform.position; 
+				maxDist = 1200f;
+		}
 	
-	void Update() {
-		if (Vector3.Distance(transform.position, startPosition) > maxDist)
-			GameObject.Destroy(gameObject);
-	}
+		void Update ()
+		{
+				if (Vector3.Distance (transform.position, startPosition) > maxDist)
+						GameObject.Destroy (gameObject);
+		}
 
-	void SetShooter(GameObject s) { shooter = s; }
-	void SetTarget(GameObject s) { target = s; }
+		void SetShooter (GameObject s)
+		{
+				shooter = s;
+		}
+		void SetTarget (GameObject s)
+		{
+				target = s;
+		}
 
-	void CollideWithShip(GameObject ship) {
-		if (shooter == ship) { return; }
-		int ship1Num = shooter.GetComponent<Ship> ().GetPlayerNumber ();
-		int ship2Num = ship.GetComponent<Ship> ().GetPlayerNumber ();
-		if ((ship1Num == 1 && ship2Num == 3) ||
-		    (ship1Num == 3 && ship2Num == 1) ||
-		    (ship1Num == 2 && ship2Num == 4) ||
-		    (ship1Num == 4 && ship2Num == 2))
-			return;
+		void CollideWithShip (GameObject ship)
+		{
+				if (shooter == ship) {
+						return;
+				}
+				int ship1Num = shooter.GetComponent<Ship> ().GetPlayerNumber ();
+				int ship2Num = ship.GetComponent<Ship> ().GetPlayerNumber ();
+				if ((ship1Num == 1 && ship2Num == 3) ||
+						(ship1Num == 3 && ship2Num == 1) ||
+						(ship1Num == 2 && ship2Num == 4) ||
+						(ship1Num == 4 && ship2Num == 2))
+						return;
 
-		var otherPos = ship.transform.position;
-		var knockDirection = (transform.position - otherPos).normalized;
-		ship.SendMessage("Damage");
-		Instantiate(explosion, otherPos, Quaternion.identity);
-		Destroy(gameObject);
-		ship.rigidbody.AddForce(knockDirection * 100f);
-	}
+				var otherPos = ship.transform.position;
+				var knockDirection = (transform.position - otherPos).normalized;
+				ship.SendMessage ("Damage");
+				Instantiate (explosion, otherPos, Quaternion.identity);
+				Destroy (gameObject);
+				ship.rigidbody.AddForce (knockDirection * 100f);
+		}
 }
