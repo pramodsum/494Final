3b3757d92113c9e216fc3a0f0799c0c74db60695
Sumedha <pramods@umich.edu>
2014-11-24 20:17:05 -0500

    initial menu setup

--- Assets/Scripts/MainMenuScript.cs
+++ Assets/Scripts/MainMenuScript.cs
@@ -1,29 +1,29 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class MainMenuScript : MonoBehaviour {
+public class MainMenuScript : MonoBehaviour
+{
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
-
-	void OnGUI()
-	{
-		Rect playButton = new Rect (Screen.width / 2 - 50, Screen.height / 2 - 25, 100, 50);
-		if ( GUI.Button(playButton, "Play - 4 Players") )
+		// Use this for initialization
+		void Start ()
 		{
-			Application.LoadLevel("_Scene_2_Duncan_CTF 1");
+	
 		}
-		Rect playButton2 = new Rect (Screen.width / 2 - 50, Screen.height / 2 - 25 + 100, 100, 50);
-		if ( GUI.Button(playButton2, "Play - 2 Players") )
+	
+		// Update is called once per frame
+		void Update ()
 		{
-			Application.LoadLevel("_Scene_2_Duncan_CTF 2 Players");
+		
+	
 		}
-	}
+		
+		public void StartTwoPlayer ()
+		{
+				Application.LoadLevel ("_Scene_2_Duncan_CTF 2 Players");
+		}
+	
+		public void StartFourPlayer ()
+		{
+				Application.LoadLevel ("_Scene_2_Duncan_CTF 1");
+		}
 }
