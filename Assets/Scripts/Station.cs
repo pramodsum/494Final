4de09d402f2505bc288c005044b597fa23d2320a
Evan Hahn <me@evanhahn.com>
2014-12-08 08:32:45 -0500

    Missile coloring
    Closes #105.

--- Assets/Scripts/Station.cs
+++ Assets/Scripts/Station.cs
@@ -6,7 +6,7 @@ public class Station : MonoBehaviour
 		public Station_Control station;
 		void OnTriggerEnter (Collider other)
 		{
-				if (other.gameObject.name == "Shot" /*&& !station.inControl*/) {
+				if ((other.gameObject.name == "Shot") || ((other.gameObject.name == "Missile")) /*&& !station.inControl*/) {
 						this.renderer.material.color = other.renderer.material.color; 
 				}
 		}
