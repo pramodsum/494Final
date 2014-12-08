dab6cbb7530bbba3d67b128f59380ecd73e707a0
Sumedha <pramods@umich.edu>
2014-12-08 15:46:43 -0500

    Fixed ship 2 particle system + access to credits scene

---
+++ Assets/Scripts/CreditsScript.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CreditsScript : MonoBehaviour
+{
+
+		// Use this for initialization
+		void Start ()
+		{
+	
+		}
+	
+		// Update is called once per frame
+		void Update ()
+		{
+	
+		}
+	
+		public void ShowMainMenu ()
+		{
+				Application.LoadLevel ("_Scene_Main_Menu");
+		}
+}
