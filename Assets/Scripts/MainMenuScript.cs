62cbb70e6410f7d50256f5c73bedc404f6da29c6
Sumedha <pramods@umich.edu>
2014-12-08 19:29:03 -0500

    Moved credits stuff to main menu scene itself
    keeps music running throughout

--- Assets/Scripts/MainMenuScript.cs
+++ Assets/Scripts/MainMenuScript.cs
@@ -7,14 +7,7 @@ public class MainMenuScript : MonoBehaviour
 		// Use this for initialization
 		void Start ()
 		{
-	
-		}
-	
-		// Update is called once per frame
-		void Update ()
-		{
-		
-	
+				GameObject.Find ("Credits").GetComponent<CanvasGroup> ().alpha = 0;
 		}
 		
 		public void StartTwoPlayer ()
@@ -25,5 +18,17 @@ public class MainMenuScript : MonoBehaviour
 		public void StartFourPlayer ()
 		{
 				Application.LoadLevel ("_Scene_2_Duncan_CTF 1");
+		}
+	
+		public void ShowCredits ()
+		{
+				GameObject.Find ("Credits").GetComponent<CanvasGroup> ().alpha = 1;
+				GameObject.Find ("MainMenu").GetComponent<CanvasGroup> ().alpha = 0;
+		}
+	
+		public void ShowMainMenu ()
+		{
+				GameObject.Find ("MainMenu").GetComponent<CanvasGroup> ().alpha = 1;
+				GameObject.Find ("Credits").GetComponent<CanvasGroup> ().alpha = 0;
 		}
 }
